4246701c45c049e9eddc8dd1bd0081f6570e936b
NamhoGim <kkjknh2@gmail.com>
2017-02-22 13:12:32 +0900

    update install.sh.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # soft link for vim
 
 PACAKGE=("texlive" "vim" "subversion" "openssh-server" "tmux" "exuberant-ctags" "tree")
-TARGET=(".vimrc" ".bashrc" ".vim" ".subversion" ".ssh" ".tmux.conf")
+TARGET=(".vimrc" ".bash_profile" ".bashrc" ".vim" ".subversion" ".ssh" ".tmux.conf")
 
 for t in ${TARGET[@]}; do
     rm -rf $HOME/$t
@@ -11,7 +11,7 @@ for t in ${TARGET[@]}; do
     echo $t" is changed"
 done
 
-sudo apt-get install subversion
+#sudo apt-get install subversion
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +BundleInstall +qall
+vim +PluginInstall +qall
